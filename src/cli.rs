668977983469a03d13d7a9e892c52c5589cad685
Martin Vilcans <martin@librador.com>
2021-03-20 17:26:27 +0100

    Possible to export image from command-line

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 //! Command-line interface
 
-use pixel_pen::{storage, Application};
+use pixel_pen::{error::Error, storage, Application, Document};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -10,6 +10,9 @@ struct Opts {
     /// File to load
     #[structopt(parse(from_os_str))]
     filename: Option<PathBuf>,
+    /// Save the image to the given file and quit.
+    #[structopt(long = "--export-file", requires = "filename")]
+    export_file: Option<PathBuf>,
 }
 
 /// Parses command-line arguments and prints any errors, returns Application ready to start.
@@ -17,18 +20,46 @@ struct Opts {
 /// On error, returns the exit code for `process::exit`.
 pub fn main() -> Result<Option<Application>, i32> {
     let opts = Opts::from_args();
-    let app = if let Some(filename) = opts.filename {
-        let doc = storage::load_any_file(&filename).map_err(|err| {
+    let doc = if let Some(filename) = &opts.filename {
+        Some(storage::load_any_file(filename).map_err(|err| {
             eprintln!(
                 "Could not load file {}: {}",
                 filename.to_string_lossy(),
                 err
             );
             1
-        })?;
-        Application::with_doc(doc)
+        })?)
     } else {
-        Application::default()
+        None
     };
-    Ok(Some(app))
+    match execute_commands(&opts, doc.as_ref()) {
+        Err(err) => {
+            eprintln!("Command failed: {}", err);
+            Err(2)
+        }
+        Ok(true) => Ok(None),
+        Ok(false) => {
+            if let Some(doc) = doc {
+                Ok(Some(Application::with_doc(doc)))
+            } else {
+                Ok(Some(Application::default()))
+            }
+        }
+    }
+}
+
+/// Returns Ok(true) if a command was executed and the app should quit.
+/// Returns Ok(false) if the app should start the GUI.
+fn execute_commands(opts: &Opts, doc: Option<&Document>) -> Result<bool, Error> {
+    let mut executed = false;
+    if let Some(filename) = &opts.export_file {
+        match storage::save(doc.unwrap(), filename) {
+            Ok(()) => executed = true,
+            Err(e) => {
+                eprintln!("Failed to save: {:?}", e);
+                return Err(e);
+            }
+        }
+    }
+    Ok(executed)
 }
