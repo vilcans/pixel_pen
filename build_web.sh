64bcaaf8712148b6d6747b493c878c3096bb1603
Martin Vilcans <martin@librador.com>
2021-04-04 01:16:47 +0200

    Some cleanup from the assumption folder_name == crate name

--- build_web.sh
+++ build_web.sh
@@ -3,8 +3,7 @@ set -eu
 
 # ./setup_web.sh # <- call this first!
 
-FOLDER_NAME=pixel_pen
-CRATE_NAME=$FOLDER_NAME # assume crate name is the same as the folder name
+CRATE_NAME=pixel_pen
 
 # This is required to enable the web_sys clipboard API which egui_web uses
 # https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.Clipboard.html
