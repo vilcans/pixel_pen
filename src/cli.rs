edf45130e474fb3a4acd0c1ee07ede588dc99683
Martin Vilcans <martin@librador.com>
2021-11-12 23:53:51 +0100

    Happy clippy: unwrap_or_default

--- src/cli.rs
+++ src/cli.rs
@@ -43,7 +43,7 @@ pub fn main() -> Result<Option<Application>, i32> {
         }
         Ok(true) => Ok(None),
         Ok(false) => {
-            let doc = doc.unwrap_or_else(Document::default);
+            let doc = doc.unwrap_or_default();
             let mut app = Application::with_doc(doc);
             if let Some(filename) = opts.import_file {
                 match app.start_import_mode(&filename) {
