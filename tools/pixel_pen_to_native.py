9a935ec3bb1d5b7ea24a52b155ed126caccdcb7f
Martin Vilcans <martin@librador.com>
2021-12-04 19:19:49 +0100

    pixel_pen_to_native.py can write metadata to file

--- tools/pixel_pen_to_native.py
+++ tools/pixel_pen_to_native.py
@@ -7,16 +7,35 @@ Writes a file with the video array (character numbers) followed by the character
 
 import json
 import codecs
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Colors:
+    background: int
+    border: int
+    aux: int
+
+@dataclass
+class Image:
+    colors: Colors
+    width: int
+    height: int
+    char_height: int
+    num_chars: int
+    video: List[int]
+    video_colors: List[int]
+    bitmaps: List[int]
 
 
 def parse_char(char_hex):
     return codecs.decode(char_hex, 'hex')
 
 
-def convert(document, left, top, width, height):
+def convert(document, left, top, width, height) -> Image:
     """Convert the given document to native bytes.
 
-    Returns the video array, color array, and the bitmaps.
+    Returns the Image object.
     """
 
     image = document['image']
@@ -54,15 +73,27 @@ def convert(document, left, top, width, height):
         for r in range(top, top + height)
         for c in video_chars[r * columns + left:r * columns + left + width]
     )
-    video_colors = image['video-colors']
-    colors = bytes(
+    video_colors = bytes(
         c
         for r in range(top, top + height)
-        for c in video_colors[r * columns + left:r * columns + left + width]
+        for c in image['video-colors'][r * columns + left:r * columns + left + width]
     )
 
     assert len(video) == width * height
-    return (video, colors, bitmaps)
+    return Image(
+        colors=Colors(
+            background=image['colors'][0],
+            border=image['colors'][1],
+            aux=image['colors'][2],
+        ),
+        width=width,
+        height=height,
+        num_chars=len(characters),
+        char_height=8,
+        video=video,
+        video_colors=video_colors,
+        bitmaps=bitmaps
+    )
 
 
 def main():
@@ -75,6 +106,8 @@ def main():
     parser.add_argument('--top', type=int, help='Include source image starting at this character row')
     parser.add_argument('--width', type=int, help='Number of character columns to include')
     parser.add_argument('--height', type=int, help='Number of character rows to include')
+    parser.add_argument('--meta', help='Write metadata to this file')
+    parser.add_argument('--meta-prefix', default='', help='When writing metadata file, add this prefix to the beginning of each symbol')
     parser.add_argument(
         'input', type=argparse.FileType('rb'),
         help='Pixel Pen file to convert'
@@ -87,11 +120,29 @@ def main():
     args = parser.parse_args()
 
     document = json.load(args.input)
-    (video, colors, bitmaps) = convert(document, args.left, args.top, args.width, args.height)
+    image = convert(document, args.left, args.top, args.width, args.height)
     with open(args.output, 'wb') as out:
-        out.write(video)
-        out.write(colors)
-        out.write(bitmaps)
+        out.write(image.video)
+        out.write(image.video_colors)
+        out.write(image.bitmaps)
+    if args.meta:
+        video_size = image.width * image.height
+        with open(args.meta, 'w') as out:
+            for line in [
+                f'{args.meta_prefix}width = {image.width}',
+                f'{args.meta_prefix}height = {image.height}',
+                f'{args.meta_prefix}background = {image.colors.background}',
+                f'{args.meta_prefix}border = {image.colors.border}',
+                f'{args.meta_prefix}aux = {image.colors.aux}',
+                f'{args.meta_prefix}num_chars = {image.num_chars}',
+                f'{args.meta_prefix}video_offset = 0',
+                f'{args.meta_prefix}colors_offset = {video_size}',
+                f'{args.meta_prefix}video_size = {video_size}',
+                f'{args.meta_prefix}bitmaps_offset = {video_size * 2}',
+                f'{args.meta_prefix}bitmaps_size = {image.num_chars * image.char_height}',
+            ]:
+                out.write(line)
+                out.write('\n')
 
 
 if __name__ == '__main__':
