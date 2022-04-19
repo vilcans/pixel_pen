941a950ea2e0e8ac7a2044551387c39fb25610a7
Martin Vilcans <martin@librador.com>
2022-04-19 22:33:31 +0200

    Crosshair more consistent

--- src/ui/crosshair.rs
+++ src/ui/crosshair.rs
@@ -7,14 +7,14 @@ const STROKE: Stroke = Stroke {
     color: Color32::from_rgb(200, 200, 200),
 };
 
+/// Draw a crosshair on the given pixel point.
+/// The point is clamped to inside the image, or on the rightmost/bottommost edge.
+/// (Because user may want to select everything starting from the right/bottom.)
 pub fn draw_crosshair(painter: &Painter, pixel_transform: &PixelTransform, pos: PixelPoint) {
-    if pos.x < 0
-        || pos.x >= pixel_transform.pixel_width
-        || pos.y < 0
-        || pos.y >= pixel_transform.pixel_height
-    {
-        return;
-    }
+    let pos = pos.clamp(
+        PixelPoint::origin(),
+        PixelPoint::new(pixel_transform.pixel_width, pixel_transform.pixel_height),
+    );
     painter.line_segment(
         [
             pixel_transform.screen_pos(PixelPoint::new(pos.x, 0)),
